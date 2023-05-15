8eadbe1df6962016ce441753030b30928601baae
Vyacheslav Bereza <vyacheslav.bereza@gmail.com>
2023-05-15 11:46:45 +0300

    first version

---
+++ news/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import News
+
+
+# Create your views here.
+from .models import News
+
+def news(request):
+    newss = News.objects.all().order_by('-date')
+    return render(request, 'news.html', {'newss': newss})
